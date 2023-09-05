ae11bfbbbed784dfbf831f005c500b72862e54a3
Nicky Jarvis <nicola.jarvis@hotmail.co.uk>
2023-09-05 18:04:32 +0100

    First edit of the three mixed message files

---
+++ script.js
@@ -0,0 +1,38 @@
+//Here is my JavaScript file to go alongside my mixed messages page and accompany my html and css code for that page.
+
+const starSign = ["Aries", "Leo", "Sagittarius", "Taurus", "Virgo", "Capricorn", "Gemini", "Libra", "Aquarius"];
+
+const earth = ["Taurus", "Virgo", "Capricorn"];
+const fire = ["Aries", "Leo", "Sagittarius"];
+const air = ["Gemini", "Libra", "Aquarius"];
+const water = ["Cancer", "Scorpio", "Pisces"];
+
+function show(starSign) {
+    document.querySelector(".text-box").value = value;
+}
+
+let dropdown = document.querySelector(".dropdown")dropdown.onclick = function(); {
+    dropdown.classList.toggle("active")
+}
+
+/* <div class = "dropdown">
+<div id="myDropdown" class = "dropdown-content">
+    <button onclick = "myFunction()" class = "dropbtn">Dropdown
+    <a href="#">Aquarius</a>
+    <a href="#">Pisces</a>
+    <a href="#">Aries</a>
+    <a href="#">Taurus</a>
+    <a href="#">Gemini</a>
+    <a href="#">Cancer</a>
+    <a href="#">Leo</a>
+    <a href="#">Virgo</a>
+    <a href="#">Libra</a>
+    <a href="#">Scorpio</a>
+    <a href="#">Sagittarius</a>
+    <a href="#">Capricorn</a>
+    </button>
+</div>
+</div> */
+
+
+
