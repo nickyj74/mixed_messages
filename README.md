5174bbac723a7251d5d9a597c77b34efed12d443
Nicky <134431774+nickyj74@users.noreply.github.com>
2024-01-25 20:22:19 +0000

    dropdown menu selection + edits

---
+++ README.md
@@ -0,0 +1,39 @@
+# Mixed Messages - Portfolio Project #1
+
+
+## Description: 
+This is my first portfolio project from the Codecademy full-stack engineer course. 
+The goal is to create message generator program using JavaScript.
+To use Git version control
+Develop locally on my own computer.
+Each time a user runs the program, they should return a new, and randomised, output.
+The message should be made up of at least three different pieces of data.
+
+I have decided to create a motivational program, and so the returned output will reflect this.
+
+
+## Features:
+- Home page in HTML
+- CSS styling
+- Dropdown Menu in JS
+- Option selector in JS
+- Randomised quotes in JS
+- Static images for style purposes
+
+
+## How to use:
+The user will click on a button, to choose their star sign, which when elicit a click event to generate a random quote.
+
+
+## Technologies:
+> JavaScript
+> HTML
+> CSS
+
+
+## Collaborators:
+Project completed by myself - Nicky Jarvis
+
+
+## License:
+None.
--- script.js
+++ script.js
@@ -1,32 +1,7 @@
-// starSign dictionary...
-const starSign = {
-  "Aries" : "fire",
-  "Leo" : "fire",
-  "Sagittarius" : "fire",
-  "Taurus" : "earth",
-  "Virgo" : "earth",
-  "Capricorn" : "earth",
-  "Gemini" : "air",
-  "Libra" : "air",
-  "Aquarius" : "air",
-  "Cancer" : "water",
-  "Scorpio" : "water",
-  "Pisces" : "water"
-}
+// List of Star Signs:
+const starSign = ["Aries", "Leo", "Sagittarius", "Taurus", "Virgo", "Capricorn", "Gemini", "Libra", "Aquarius", "Cancer", "Scorpio", "Pisces"];
 
-/*
-const fire = (starSign [0, 1, 2]);
-//const fire = ["Aries", "Leo", "Sagittarius"];
-const earth = (starSign [3, 4, 5]);
-//const earth = ["Taurus", "Virgo", "Capricorn"];
-const air = (starSign [6, 7, 8]);
-//const air = ["Gemini", "Libra", "Aquarius"];
-const water = (starSign [9, 10, 11]);
-//const water = ["Cancer", "Scorpio", "Pisces"]; 
-*/
-
-
-//Create an array of quotes...
+// Array of quotes to be randomised:
 var quotes = [
   "You cannot just sit there and wait for people to give you that golden dream. You have to go out there and make it happen. - Diana Ross", 
   "Embrace your inner lion, for a Leo's roar has the power to inspire, uplift and ignite the fire within others. - Graciousquotes.com", 
@@ -39,69 +14,24 @@ var quotes = [
   "Concentrate all your thoughts upon the work in hand. The sun's rays do not burn until brought to a focus. - Alexander Graham Bell",
   "The most difficult thing is the decision to act, the rest is merely tenacity. - Amelia Earhart",
   "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence. - Helen Keller",
-  "Just one small positive thought in the morning can change your whole day. - Dalai Lama" 
-]
+  "Just one small positive thought in the morning can change your whole day. - Dalai Lama",
+];
 
-//Randomise the quotes...
-function newQuote() {
-  var randomQuote = Math.floor(Math.random() * (quotes.length));
-  document.getElementById('quoteDisplay').innerHTML = quotes[randomQuote];
-}
-
-
-
-//listed below are variable declarations for the button to work - hopefully.
-const dropdownBtn = document.getElementById("btn");
-const dropdownMenu = document.getElementById("dropdown");
-const toggleArrow = document.getElementById("arrow");
-
-const toggleDropdown = function () {
-    dropdownMenu.classList.toggle("show");
-    toggleArrow.classList.toggle("arrow");
-  };
-
-  dropdownBtn.addEventListener("click", function (e) {
-    e.stopPropagation();
-    toggleDropdown();
-  });
-
-  document.documentElement.addEventListener("click", function () {
-    if (dropdownMenu.classList.contains("show")) {
-      toggleDropdown();
-    }
-  });
-
-
-// !!DO I NEED THIS --- need to check?!
-//Function to enable chosen sign to be saved into a variable...
-/*function setSign () {
-  var chosenSign = document.getElementById("myDropdown").value;
-  }
- */ 
-
-/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
+// When the user clicks on the button, toggle between hiding and showing the dropdown content
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
+  };
 
+//When the user selects a starsign the chosen option stays visible
+let selection = document.querySelector('select');
+let result = document.querySelector('h2');
+  
+selection.addEventListener('change', () => {
+    result.innerHTML = selection.options[selection.selectedIndex].text;
+});
 
-/// Now I need to create an if/else so that when the options are made I can then push them into their catagories.
-function sendMessage () {
-  if (starSign = "fire") {
-    console.log("Hello ${name}, your Starsign is a fire element, so we have selected the following quote for you today... " + "${newQuote}");
-  } else if (starSign = "earth") {
-      console.log("Hello ${name}, your Starsign is an earth element, so we have selected the following quote for you today... " + "${newQuote}");
-    } else if (starSign = "air") {
-       console.log("Hello ${name}, your Starsign is an air element, so we have selected the following quote for you today... " + "${newQuote}");
-      } else {
-        console.log("Hello ${name}, your Starsign is a water element, so we have selected the following quote for you today... " + "${newQuote}");
-      }
-    }
-
-
-  // !! STUCK HERE!! - Trying to create a final string message with interpolations of the user name, starsign and their random quote...
-  document.getElementById("myDropdown").onchange = function () {
-    alert(this.value);
-  }
-//or ??
-document.getElementById("setSign", sendMessage);
+// !! STUCK HERE!! - Trying to generate a quote with the selected option in the dropbtn...
+dropbtn.addEventListener('click', function(){
+  var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+  dropbtn.innerHTML = randomQuote;
+});
